92add2448f8508793789b3f4f29273bab476a2f3
Noise_dev <vvv55019@gmail.com>
2025-11-11 11:36:49 +0500

    BuildPrefab Update

--- Mechanics/Prefab/BuildPrefab.cs
+++ Mechanics/Prefab/BuildPrefab.cs
@@ -1,38 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildPrefab : MonoBehaviour {
-    public Collider[] colliders;
-    public MeshFilter[] meshes;
-    public Component[] rends;
-    public List<Material> materials = new List<Material>();
-    public Material canM;
-    public Vector3 ppos;
-    public Vector3 prot;
+public class BuildPrefab : MonoBehaviour 
+{
+    private Collider[] colliders;
+    private Renderer[] renderers;
+    private Material[] materials;
+    [SerializeField] private  Material canM;
+    [SerializeField] private Vector3 ppos;
+    [SerializeField] private Vector3 prot;
 
-    private void Start() {
-        colliders = GetComponentsInChildren<Collider>();
-        rends = GetComponentsInChildren<Renderer>();
-        meshes = GetComponentsInChildren<MeshFilter>();
-        for (int i = 0; i < colliders.Length; i++) {
-            colliders[i].enabled = false;
-        }
-        for (int i = 0; i < rends.Length; i++) {
-            materials.Add(rends[i].GetComponent<Renderer>().material);
-            meshes[i].GetComponent<Renderer>().material = canM;
-        }
-    }
-    public bool Place(Vector3 pos, Vector3 local) {
-        transform.position = pos + ppos;
-        transform.localEulerAngles = local + prot;
-        // meshes.GetComponent<Renderer>().material = materials[i];
-        for (int i = 0; i < colliders.Length; i++) {
+    public bool Place(Vector3 _pos, Vector3 _rot)
+    {
+        transform.position = _pos + ppos;
+        transform.localEulerAngles = _rot + prot;
+        
+        for (int i = 0; i < colliders.Length; i++)
+        {
             colliders[i].enabled = true;
         }
-        for (int i = 0; i < materials.Count; i++) {
-            meshes[i].GetComponent<Renderer>().material = materials[i];
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material = materials[i];
         }
+        
         Destroy(this);
         return true;
+    }
+
+    private void Start()
+    {
+        colliders = GetComponentsInChildren<Collider>();
+        renderers = GetComponentsInChildren<Renderer>();
+        
+        materials = new Material[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            materials[i] = renderers[i].material;
+            renderers[i].material = canM;
+        }
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].enabled = false;
+        }
     }
 }
